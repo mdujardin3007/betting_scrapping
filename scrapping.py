626cd4f19155d89e2c7c6b10322b95900068afe6
TonNomGit <ton_email@example.com>
2025-07-08 23:25:50 +0200

    Mise à jour des données valuebets

--- scrapping.py
+++ scrapping.py
@@ -229,8 +229,7 @@ async def main_scrape_and_notify():
             else:
                 df_final = df_existing.copy()
 
-            print(df_final.head())
-            df_final = df_final.reset_index(drop=True)  # Remettre un index simple
+            df_final = df_final.drop(columns=["event_short"])  # Remettre un index simple
 
             df_final.to_excel(DATABASE_FILE, index=False)
             print(f"✅ Données sauvegardées dans {DATABASE_FILE} avec {len(df_final)} entrées.")
